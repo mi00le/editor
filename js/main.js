28697a349b0cf83f34b486a17ab10c5526d43f28
Michael Lindfors <michael.lindfors@hotmail.com>
2023-03-01 12:12:48 +0200

    navbar section updated

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ class Editor {
     constructor() {
         this.field = document.querySelector(".field");
         this.typed = [];
+        this.modal_flag = false;
     }
     init() {
         this.field.innerHTML = "&#9615;";
         this.handle_keys();
+
         document.querySelector("#new").addEventListener("click", this.new_doc, false);
         document.querySelector("#open").addEventListener("click", this.open_doc, false);
         document.querySelector("#save").addEventListener("click", this.save_doc, false);
@@ -16,7 +18,7 @@ class Editor {
         document.body.addEventListener(
             "keydown",
             (e) => {
-                if (e.type == "keydown") {
+                if (e.type == "keydown" && this.modal_flag == false) {
                     console.log(this.typed);
                     this.handle_chars(e.key);
                     this.handle_special_keys(e.key);
@@ -83,31 +85,55 @@ class Editor {
         this.typed = [];
     };
     open_doc = () => {
+        this.modal_flag = true;
         this.typed = [];
         document.querySelector(".field").innerHTML = "";
-        fs.readFile("test.txt", "utf8", (err, data) => {
-            if (err) {
-                console.error(err);
-                return;
-            }
+        document.querySelector(".modal").style.display = "block";
+        document.querySelector("#confirm").addEventListener(
+            "click",
+            () => {
+                let filename = document.querySelector("#filename").value;
 
-            document.querySelector(".field").innerHTML = data + "&#9615;";
-            data = data.split("");
-            data.map((e) => {
-                this.typed.push(e);
-            });
-        });
+                fs.readFile(filename, "utf8", (err, data) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    document.querySelector(".modal").style.display = "none";
+                    this.modal_flag = false;
+
+                    document.querySelector(".field").innerHTML = data + "&#9615;";
+                    data = data.split("");
+                    data.map((e) => {
+                        this.typed.push(e);
+                    });
+                    document.querySelector("#filename").value = "";
+                });
+            },
+            false
+        );
     };
     save_doc = () => {
+        this.modal_flag = true;
         const content = document.querySelector(".field").innerHTML.split("").slice(0, this.typed.length).join("");
-
-        fs.writeFile("test.txt", content, (err) => {
-            if (err) {
-                console.error(err);
-            }
-            // file written successfully
-            console.log("successfully saved!");
-        });
+        document.querySelector(".modal").style.display = "block";
+        document.querySelector("#confirm").addEventListener(
+            "click",
+            () => {
+                let filename = document.querySelector("#filename").value;
+                fs.writeFile(filename, content, (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    // file written successfully
+                    console.log("successfully saved!");
+                    document.querySelector(".modal").style.display = "none";
+                    this.modal_flag = false;
+                    document.querySelector("#filename").value = "";
+                });
+            },
+            false
+        );
     };
 }
 
@@ -115,5 +141,4 @@ let editor = new Editor();
 
 document.body.onload = () => {
     editor.init();
-    // navbar.init();
 };
